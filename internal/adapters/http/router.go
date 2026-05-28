cb9f466466828f3a8c7926734ddc780f558929a6
fabricioricard <fabricioricard23@gmail.com>
2026-05-28 14:14:18 -0300

    Add health check route

--- internal/adapters/http/router.go
+++ internal/adapters/http/router.go
@@ -1,14 +1,32 @@
 ﻿package http
 
 import (
-	"github.com/go-chi/chi/v5"
-	"go.uber.org/zap"
-	"github.com/fabricioricard/nkn-defi/internal/ports/cache"
-	"github.com/fabricioricard/nkn-defi/internal/app"
+    "net/http"
+
+    "github.com/go-chi/chi/v5"
+    "go.uber.org/zap"
+
+    "github.com/fabricioricard/nkn-defi/internal/app"
+    "github.com/fabricioricard/nkn-defi/internal/ports/cache"
 )
 
-func NewRouter(poolUC *app.PoolUsecase, swapUC *app.SwapUsecase, stakeUC *app.StakingUsecase, nodeUC *app.NodeUsecase, logg *zap.Logger, cache cache.Cache) chi.Router {
-	r := chi.NewRouter()
-	// rotas serão adicionadas depois
-	return r
+func NewRouter(
+    poolUC *app.PoolUsecase,
+    swapUC *app.SwapUsecase,
+    stakeUC *app.StakingUsecase,
+    nodeUC *app.NodeUsecase,
+    logg *zap.Logger,
+    cache cache.Cache,
+) chi.Router {
+    r := chi.NewRouter()
+
+    // Health check
+    r.Get("/healthz", func(w http.ResponseWriter, r *http.Request) {
+        w.WriteHeader(http.StatusOK)
+        w.Write([]byte("OK"))
+    })
+
+    // Futuras rotas virão aqui...
+
+    return r
 }
